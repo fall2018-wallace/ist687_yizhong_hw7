a9608d097a0257d5689e5b926e8762e793b26576
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 19:40:01 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -8,7 +8,7 @@ readStates<-function(states)
     colnames(states)<-c("stateName","population","popOver18","percentOver18")
     return(states)
 }
-clean_data<-readStates(raw_data)
+data<-readStates(raw_data)
 arrests<-data.frame(USArrests)
 arrests$stateName<-row.names(arrests)
 merge_df<-merge(arrests,data,by="stateName")
