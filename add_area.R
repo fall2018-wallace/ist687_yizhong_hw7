e61dd4fbc28b7567cc76832e462650a08baadfc5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 19:58:02 -0400

    Saving add_area.R

--- add_area.R
+++ add_area.R
@@ -4,3 +4,4 @@ stateName<-state.name
 stateCenter<-state.center
 state_area_center<- data.frame(stateArea, stateCenter, stateName)
 merge_df<- merge(merge_df, state_area_center, by="stateName")
+merge_df$stateName<-tolower(merge_df$stateName)
