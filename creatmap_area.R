667eff37e760babeca1208efa2557f70d71015cb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 19:57:39 -0400

    Saving creatmap_area.R

--- creatmap_area.R
+++ creatmap_area.R
@@ -2,6 +2,6 @@
 library(ggplot2)
 library(ggmap)
 library(maps)
-merge_df$stateName<-tolower(merge_df$stateName)
+
 us<- map_data("state")
 map_area<-ggplot(merge_df,aes(map_id=stateName))+geom_map(map=us, aes(fill=stateArea))+expand_limits(x = us$long, y = us$lat)+coord_map() + ggtitle("state Area")
