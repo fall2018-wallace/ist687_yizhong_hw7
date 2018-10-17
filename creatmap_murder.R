f472a4841e134ed0e67c974a7c99d0b734bc9252
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 20:01:00 -0400

    Saving creatmap_murder.R

--- creatmap_murder.R
+++ creatmap_murder.R
@@ -1,3 +1,3 @@
 
-library(ggplot)
+library(ggplot2)
 map_murder<-ggplot(merge_df,aes(map_id=stateName))+geom_map(map=us, aes(fill=Murder))+expand_limits(x = us$long, y = us$lat)+coord_map() + ggtitle("Murder Rate")
