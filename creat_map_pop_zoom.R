48504284aac0e7674cf1d329c148d23b30990d5c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 21:43:56 -0400

    Saving creat_map_pop_zoom.R

--- creat_map_pop_zoom.R
+++ creat_map_pop_zoom.R
@@ -1,5 +1,7 @@
 
 library(ggplot2)
+library(ggmap)
+library(maps)
 us<- map_data("state")
 map_murder<-ggplot(merge_df,aes(map_id=stateName))+geom_map(map=us, aes(fill=Murder))+expand_limits(x = us$long, y = us$lat)+coord_map() + ggtitle("Murder Rate")
 map_pop<-map_murder+geom_point(data = merge_df,aes(x=merge_df$x,y=merge_df$y,size=merge_df$population))
